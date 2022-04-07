79ccade6f55d804ce76b953ec75c1dc552df471d
Ngoc Nguyen 8 <ngoc.nguyen8@sotatek.com>
2022-04-07 11:42:07 +0700

    fix: set env

--- phase1/index.js
+++ phase1/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 async function main() {
   var Web3 = require("web3");
   var web3 = new Web3();
+
+  const PRIVATE_KEY = process.env.PRIVATE_KEY;
   const provider =
     "wss://rinkeby.infura.io/ws/v3/710b741fe9924cc8a5fa4fa20a89e620";
   web3.setProvider(new web3.providers.WebsocketProvider(provider));
@@ -17,7 +20,7 @@ async function main() {
 
   const signedTx = await web3.eth.accounts.signTransaction(
     transaction,
-    "a1efbe9b745a81bcb6a5beab92476169b54b182a7b2b393b9bd262dbca5e3d8a"
+    PRIVATE_KEY
   );
 
   web3.eth.sendSignedTransaction(
